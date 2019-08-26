4c6c76b4e725c6316b6b8792cf04967fb34eeda9
ChuongTran <chuongtrh@gmail.com>
2019-08-26 13:56:10 +0700

    ~ Add report test

--- script/test.sh
+++ script/test.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-./node_modules/.bin/mocha ./test/test.js --exit
+#./node_modules/.bin/mocha ./tests/** --exit
+MOCHA_FILE=./build/reports/jenkins-test-results.xml ./node_modules/.bin/mocha tests/** --reporter mocha-junit-reporter --exit
---
+++ tests/demo.js
@@ -0,0 +1,9 @@
+
+describe('Demo', function () {
+    it('Function 1', function (done) {
+        done();
+    });
+    it('Function 2', function (done) {
+        done();
+    });
+});
